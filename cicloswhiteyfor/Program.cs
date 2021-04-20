7cfb95375bab40262e1c93e44b1edb10671d6c62
arnoldo <Mauri@LAPTOP-0Q9ADC5L>
2021-04-20 15:39:31 -0700

    CicloFor

--- cicloswhiteyfor/Program.cs
+++ cicloswhiteyfor/Program.cs
@@ -16,16 +16,20 @@ namespace cicloswhiteyfor
             //parte2 condicion
             //Parte3 aumento o decremento
             int tabla;
-            int i = 1; //parte1
+            //int i = 1; //parte1
 
             Console.WriteLine("Ingrese el numero de la tabla que quiere calcular");
             tabla = int.Parse(Console.ReadLine());
 
             //Parte2 
-            while (i <= 10)
+            //while (i <= 10)
+            
+                //Console.WriteLine("" + tabla * i);
+                //i++; //Parte3
+
+                for(int i = 1; i <= 10; i++)
             {
                 Console.WriteLine("" + tabla * i);
-                i++; //Parte3
             }
             Console.ReadKey();
         }
